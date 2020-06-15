4f5e6c30f55fd7509a833ca412c719c87480c155
Akashctc1994 <45253576+Akashctc1994@users.noreply.github.com>
2020-06-15 23:14:04 +0530

    End of New Color Theme

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { error } from 'protractor';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { error } from 'protractor';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService , private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,21 +19,20 @@ export class NavComponent implements OnInit {
   login()
   {
     this.authService.login(this.model).subscribe(next => {
-      console.log('logged in successfully');
-    }, error => {
-      console.log('Failed to login');
+      this.alertify.success('logged in successfully');
+    }, () => {
+      this.alertify.error('Failed to login');
     });
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout()
   {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
