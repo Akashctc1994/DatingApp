977b649b4f04d492e1b069cb1731e0d0fc27416e
Akashctc1994 <45253576+Akashctc1994@users.noreply.github.com>
2020-06-29 19:41:06 +0530

    End Of Like

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -32,6 +32,7 @@ import {MemberEditComponent} from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsaveChanges } from './_guards/prevent-unsave-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListResolver } from './_resolvers/lists.resolver';
 
 
 export function tokenGeter(){
@@ -88,6 +89,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
+      ListResolver,
       PreventUnsaveChanges,
       {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
    ],
--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { Pagination, PaginatedResult } from '../_models/Pagination';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { UserService } from '../_services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lists',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListsComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  pagination: Pagination;
+  likesParam: string;
+  constructor( private authService: AuthService, private alertify: AlertifyService,
+    // tslint:disable-next-line: align
+    private userService: UserService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    this.route.data.subscribe(data => {
+      // tslint:disable-next-line: no-string-literal
+      this.users = data['users'].result;
+      // tslint:disable-next-line: no-string-literal
+      this.pagination = data['users'].pagination;
+    });
 
+    this.likesParam = 'Likers';
+    
+  }
+  
+  loadUsers(){
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, null,this.likesParam)
+    .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
 }
